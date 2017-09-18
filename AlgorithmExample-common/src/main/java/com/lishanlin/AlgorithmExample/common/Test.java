d889581c4c31bc92a09fe9050885a4fd1a0750d2
lishanlin000 <lishanlin000@gmail.com>
2017-09-18 18:55:16 +0800

    new

---
+++ AlgorithmExample-common/src/main/java/com/lishanlin/AlgorithmExample/common/Test.java
@@ -0,0 +1,10 @@
+package com.lishanlin.AlgorithmExample.common;
+
+/**
+ * Created by lishanlin on 2017/9/18.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
